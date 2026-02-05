88b048e3feeb0cf10a851eca1705a1713df74363
AustinTTU <austnich@ttu.edu>
2026-02-05 10:21:09 -0600

    Lab01 Completed

--- lab01/src/three_sum_sorted.c
+++ lab01/src/three_sum_sorted.c
@@ -4,5 +4,27 @@ int three_sum_zero_sorted_one(const int* nums, int n, int* out_i, int* out_j, in
     
     // implement your code here
 
+    if (n < 3) return 0; // invalid input case
+
+    for (int i = 0; i < n; i++) {
+        int j = i + 1; // left pointer
+        int k = n - 1; // right pointer
+        /* Essentially do twosum problemw with negative of first index
+        to sum to total of zero */
+        while ((j < k) && (k > j)) {
+            if (nums[k] + nums[j] < (nums[i] * -1) ) {
+                j++;
+            }
+            else if (nums[k] + nums[j] > (nums[i] * -1)) {
+                k--;
+            }
+            else { // sum equal to zero, return values
+                *out_i = i;
+                *out_j = j;
+                *out_k = k;
+                return 1;
+            }
+        }
+    }
     return 0;
 }
--- lab01/src/two_sum_sorted.c
+++ lab01/src/two_sum_sorted.c
@@ -5,6 +5,21 @@ int two_sum_sorted(const int* nums, int n, int target, int* out_i, int* out_j) {
 
     // TODO: implement rwo sum solution for sorted arrays.
 
+    int i = 0; // left pointer
+    int j = n - 1; // right pointer
+    while ((i < n) && (j > 0)) {
+        if (nums[i] + nums[j] < target) { // sum too small, obtain larger i
+            i++;
+        }
+        else if (nums[i] + nums[j] > target) { // sum too big, obtain smaller j
+            j--;
+        }
+        else { // must be equal, return values
+            *out_i = i;
+            *out_j = j;
+            return 1;
+        }
+    }
     return 0;
 }
 // think about the time complexity and space complexity of your solution
