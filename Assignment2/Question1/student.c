9724ca4e351dd0889a9c0329a744774d20455f21
AustinTTU <austnich@ttu.edu>
2026-03-11 21:37:18 -0500

    Assignment2: Question 1 Completed

--- Assignment2/Question1/student.c
+++ Assignment2/Question1/student.c
@@ -29,5 +29,7 @@ struct TreeNode* bstSearch(struct TreeNode* root, int target) {
     // Hint: Use the BST property to decide whether to go left or right.
     (void)root;
     (void)target;
-    return NULL;
+    if ((root == NULL)||(root->val == target)) return root; // if node is empty (not found) or is target node, return node (either NULL or target)
+    if (target < root->val) return bstSearch(root->left, target); // recursive call into left branch (if target less than node)
+    else return bstSearch(root->right, target); // else (target greater than node), recursive call into right branch
 }
