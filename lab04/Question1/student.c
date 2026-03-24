61cf43418610ef863cf6eafa12ca4de50677f676
AustinTTU <austnich@ttu.edu>
2026-03-23 21:27:10 -0500

    Lab04: Question 1 Completed

--- lab04/Question1/student.c
+++ lab04/Question1/student.c
@@ -49,4 +49,11 @@ struct TreeNode {
 
 int maxDepth(struct TreeNode* root) {
   // TODO: implement
+  if (root == NULL) return 0;
+    else {
+        int leftHeight = maxDepth(root->left);
+        int rightHeight = maxDepth(root->right);
+        if (leftHeight > rightHeight) return leftHeight += 1;
+        else return rightHeight += 1;
+    }
 }
