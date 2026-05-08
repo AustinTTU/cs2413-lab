f904791e598b1d9c1eb5b2dd255b89c897757344
AustinTTU <austnich@ttu.edu>
2026-05-08 11:49:00 -0500

    Lab07: Question 2 Complete

--- lab07/Question2/student.c
+++ lab07/Question2/student.c
@@ -27,10 +27,49 @@ Notes:
 - If the array is empty or has only one element, do nothing.
 - You may use temporary arrays inside your merge function.
 */
+void merge(int arr[], int left, int mid, int right);
+void mergeSortRecursive(int arr[], int left, int right);
 
 void mergeSort(int arr[], int size) {
     // TODO: implement merge sort
     (void)arr;
     (void)size;
+    int left = 0;
+    int right = size-1;
+    mergeSortRecursive(arr, left, right);
 }
 
+void mergeSortRecursive(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = (left + right) /2;
+
+        mergeSortRecursive(arr, left, mid);
+        mergeSortRecursive(arr, mid+1, right);
+
+        merge(arr, left, mid, right);
+    }
+}
+
+void merge(int arr[], int left, int mid, int right) {
+    int temp[right - left + 1];
+    int i = left;
+    int j = mid + 1;
+    int k = 0;
+
+    while (i <= mid && j <= right) {
+        if (arr[i] <= arr[j]) {
+            temp[k++] = arr[i++];
+        }
+        else temp[k++] = arr[j++];
+    }
+
+    while (i <= mid) {
+        temp[k++] = arr[i++];
+    }
+    while (j <= right) {
+        temp[k++] = arr[j++];
+    }
+    for (int t = 0; t < k; t++) {
+        arr[left + t] = temp[t];
+    }
+}
