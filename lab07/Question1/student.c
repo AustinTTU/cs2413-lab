40e42c10b841b0299d7b1060a03b36ea7c2b6f3d
AustinTTU <austnich@ttu.edu>
2026-05-08 11:48:23 -0500

    Lab 07: Question 1 Complete

--- lab07/Question1/student.c
+++ lab07/Question1/student.c
@@ -32,11 +32,32 @@ void bubbleSort(int arr[], int size) {
     // TODO: implement basic bubble sort
     (void)arr;
     (void)size;
+    for (int i = 0; i < size - 1; i++) {
+        for (int j = 0; j < size-i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 
 void bubbleSortOptimized(int arr[], int size) {
     // TODO: implement optimized bubble sort with early stopping
     (void)arr;
     (void)size;
+    for (int i = 0; i < size -1 ; i++) {
+        int flag = 0;
+        for (int j = 0; j < size-i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                flag = 1;
+            }
+        }
+        if (flag == 0) return;
+    }
 }
 
