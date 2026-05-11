253ff834c5c23df613b20f02d369016c5d12db78
AustinTTU <austnich@ttu.edu>
2026-05-10 19:27:11 -0500

    Lab07: Question 4 Completed

--- lab07/Question4/student.c
+++ lab07/Question4/student.c
@@ -23,4 +23,13 @@ void insertionSort(int arr[], int size) {
     // TODO: implement insertion sort
     (void)arr;
     (void)size;
+    for (int i = 1; i < size; i++) {
+        int temp = arr[i];
+        int j = i - 1;
+        while ((temp < arr[j]) && (j >= 0)) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+    }
 }
